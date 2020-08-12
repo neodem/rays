a0afd723839764e7a4ee00f40747349e573e21c0
Vincent Fumo <neodem@gmail.com>
2020-08-12 18:45:07 -0400

    * adds cleaner angle helper

--- src/main/java/com/neodem/graphics/rays/AngleHelpers.java
+++ src/main/java/com/neodem/graphics/rays/AngleHelpers.java
@@ -1,5 +1,8 @@
 package com.neodem.graphics.rays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Vincent Fumo (neodem@gmail.com)
  * Created on 8/12/20
@@ -43,6 +46,106 @@ public class AngleHelpers {
         if (q == Quadrant.UP_LEFT || q == Quadrant.DOWN_LEFT) return Orientation.LEFT;
 
         return null;
+    }
+
+    /**
+     * will compute the points based on a traditional X/Y graph with the origin at the bottom left and
+     * Y going up and X going Right. Angles are in degrees going up to 90 with 0 being aligned with the X axis
+     * and 90 being aligned with the Y axis
+     * <p>
+     * Horizontal in this case means the first (and subsequent) whole X value axises
+     *
+     * @param size   the number of points to compute (>0)
+     * @param angle  from 0-90
+     * @param origin the starting point ( must be at 0,0 or greater)
+     * @return a List with the origin first and up to max points after
+     */
+    public static List<FloatingPoint> intersectHorizontal(FloatingPoint origin, float angle, int size) {
+        if (size <= 0) throw new IllegalArgumentException("size needs to be gt 0");
+        if (angle < 0) throw new IllegalArgumentException("angle should be positive");
+        if (angle > 90) throw new IllegalArgumentException("angle may not be greater than 90");
+        if (origin == null) throw new IllegalArgumentException("origin may not be null");
+
+        List<FloatingPoint> points = new ArrayList<>();
+        points.add(origin);
+
+        float oX = origin.getX();
+        float oY = origin.getY();
+
+        if (oX < 0) throw new IllegalArgumentException("originX may not be negative");
+        if (oY < 0) throw new IllegalArgumentException("originY may not be negative");
+
+        // compute first point!
+        float x = (int) oX + 1;
+        float dx = x - oX;
+        double tan = Math.tan(Math.toRadians(angle));
+        float y = (float) (dx * tan) + oY;
+
+        FloatingPoint firstPoint = new FloatingPoint(x, y);
+        points.add(firstPoint);
+
+        // compute other points
+        if (size > 1) {
+            FloatingPoint previousPoint = firstPoint;
+            for (int i = 0; i < size - 1; i++) {
+                float newX = previousPoint.getX() + 1;
+                float newY = (float) (previousPoint.getY() + tan);
+                previousPoint = new FloatingPoint(newX, newY);
+                points.add(previousPoint);
+            }
+        }
+
+        return points;
+    }
+
+    /**
+     * will compute the points based on a traditional X/Y graph with the origin at the bottom left and
+     * Y going up and X going Right. Angles are in degrees going up to 90 with 0 being aligned with the X axis
+     * and 90 being aligned with the Y axis
+     * <p>
+     * Vertical in this case means the first (and subsequent) whole Y value axises
+     *
+     * @param size   the number of points to compute (>0)
+     * @param angle  from 0-90
+     * @param origin the starting point ( must be at 0,0 or greater)
+     * @return a List with the origin first and up to max points after
+     */
+    public static List<FloatingPoint> intersectVertical(FloatingPoint origin, float angle, int size) {
+        if (size <= 0) throw new IllegalArgumentException("size needs to be gt 0");
+        if (angle < 0) throw new IllegalArgumentException("angle should be positive");
+        if (angle > 90) throw new IllegalArgumentException("angle may not be greater than 90");
+        if (origin == null) throw new IllegalArgumentException("origin may not be null");
+
+        List<FloatingPoint> points = new ArrayList<>();
+        points.add(origin);
+
+        float oX = origin.getX();
+        float oY = origin.getY();
+
+        if (oX < 0) throw new IllegalArgumentException("originX may not be negative");
+        if (oY < 0) throw new IllegalArgumentException("originY may not be negative");
+
+        // compute first point!
+        float y = (int) oY + 1;
+        float dy = y - oY;
+        double tan = Math.tan(Math.toRadians(90 - angle));
+        float x = (float) (dy * tan) + oX;
+
+        FloatingPoint firstPoint = new FloatingPoint(x, y);
+        points.add(firstPoint);
+
+        // compute other points
+        if (size > 1) {
+            FloatingPoint previousPoint = firstPoint;
+            for (int i = 0; i < size - 1; i++) {
+                float newY = previousPoint.getY() + 1;
+                float newX = (float) (previousPoint.getX() + tan);
+                previousPoint = new FloatingPoint(newX, newY);
+                points.add(previousPoint);
+            }
+        }
+
+        return points;
     }
 
 }
---
+++ src/test/java/com/neodem/graphics/rays/AngleHelpersTest.java
@@ -0,0 +1,39 @@
+package com.neodem.graphics.rays;
+
+import org.assertj.core.data.Offset;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by Vincent Fumo (neodem@gmail.com)
+ * Created on 8/12/20
+ */
+public class AngleHelpersTest {
+
+    @Test
+    public void intersectHorizontalShouldComputePointsCorrectly() {
+        List<FloatingPoint> floatingPoints = AngleHelpers.intersectHorizontal(new FloatingPoint(.2f, .2f), 22, 2);
+        assertThat(floatingPoints).hasSize(3);
+        assertThat(floatingPoints.get(0).getX()).isCloseTo(.2f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(0).getY()).isCloseTo(.2f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(1).getX()).isCloseTo(1f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(1).getY()).isCloseTo(.523221f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(2).getX()).isCloseTo(2f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(2).getY()).isCloseTo(.9272472f, Offset.offset(.00001f));
+    }
+
+    @Test
+    public void intersectVerticalShouldComputePointsCorrectly() {
+        List<FloatingPoint> floatingPoints = AngleHelpers.intersectVertical(new FloatingPoint(.2f, .2f), 22, 2);
+        assertThat(floatingPoints).hasSize(3);
+        assertThat(floatingPoints.get(0).getX()).isCloseTo(.2f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(0).getY()).isCloseTo(.2f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(1).getX()).isCloseTo(2.1800694f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(1).getY()).isCloseTo(1f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(2).getX()).isCloseTo(4.655156f, Offset.offset(.00001f));
+        assertThat(floatingPoints.get(2).getY()).isCloseTo(2f, Offset.offset(.00001f));
+    }
+}
